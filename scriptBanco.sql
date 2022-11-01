98e0378ec9be8b4db4bfff4575c30c20763aa884
Hibrael <hibrael.xavier@alunos.sc.senac.br>
2022-10-31 22:47:21 -0300

    Funcionalidades básicas funcionando, scrip implementado

--- scriptBanco.sql
+++ scriptBanco.sql
@@ -1,24 +1,31 @@
-﻿create database Estacionamento;
-use Estacionamento;
+﻿Create database estacionamento_db;
+Use estacionamento_db;
 
-create table tipo_Veiculo
+Create table tipo_veiculo
 (
-    id_tipo int not null,
-    descrição varchar (10)
+id_tipo int primary key not null,
+descrição varchar(9) not null
 );
 
-create table Veiculo 
+Create table Usuario
 (
-	Veiculo_id int auto_increment not null primary key, 
-	Nome varchar(25) not null, 
-    Placa varchar(9)  not null unique,
-    Modelo varchar(20) not null,
-    Hora_entrada datetime not null,
-    Hora_Saida datetime,
-    tipo_id int not null
+Username varchar(20),
+Senha varchar(10)
+);
+Create table Veiculo 
+(
+Veiculo_id int primary key auto_increment not null,
+Nome varchar(20) not null,
+Placa varchar(7) not null unique ,
+Modelo varchar(10) not null,
+Hora_entrada datetime not null,
+Hora_saida datetime,
+tipo_id int not null,
+tempo_total double,
+valor_pago double
 );
 
- INSERT INTO tipo_veiculo (id_tipo, descrição) values (1, 'moto'), (2, 'carro'), (3, 'camioneta');
+Alter table veiculo add constraint fk_veiculo_tipo foreign key (tipo_id) references tipo_veiculo (id_tipo) on delete no action on update cascade;
+INSERT INTO tipo_veiculo (id_tipo, descrição) values (1, 'moto'), (2, 'carro'), (3, 'camioneta');
 
- alter table tipo_Veiculo add constraint primary key (id_tipo);
-Alter table veiculo add constraint fk_veiculo_tipo foreign key (tipo_id) references tipo_veiculo (id_tipo) on delete no action on update cascade 
+
